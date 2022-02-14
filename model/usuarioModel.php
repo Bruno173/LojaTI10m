969e87a2333338ad3448f52d9005481676ed24c9
BRUNO GALEAZZO LUCCHESI <bruno.glucchesi@senacsp.edu.br>
2022-02-14 11:53:09 -0300

    Visualizar Usuarios

---
+++ model/usuarioModel.php
@@ -0,0 +1,23 @@
+<?php 
+
+function inserirUsuario($conn,$nomeusu,$emailusu,$foneusu,$cpfusu,$tipousu,$cepusu,$numusu,$compusu) {
+
+
+
+$query = "INSERT INTO `tbusuario` (`idusu`, `nomeusu`, `emailusu`, `foneusu`, `tipousu`, `cpfusu`, `cepusu`, `numusu`, `compleusu`) VALUES (NULL,'{$nomeusu}','{$emailusu}','{$foneusu}','{$tipousu}','{$cpfusu}','{$cepusu}','{$numusu}','{$compusu}')";
+
+
+$dados = mysqli_query($conn,$query);
+return $dados; 
+
+}
+
+function visuUsuarioNome($conn,$nomeusu){
+    $query = "select * from tbusuario where nomeusu like '{$nomeusu}'";
+    $resultado = mysqli_query($conn, $query);
+    return $resultado;
+     
+}
+
+
+?>
